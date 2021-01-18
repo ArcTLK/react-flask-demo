06767355b416eb789c9adfec073c6d298be706fe
Arc313 <nairadisi18ce@student.mes.ac.in>
2021-01-19 01:29:47 +0530

    Added tinydb docs

--- flask-demo/app.py
+++ flask-demo/app.py
@@ -1,6 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+from tinydb import TinyDB
+db = TinyDB('db.json')
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'You\'re looking at an API! Login and Register by POSTing on the /login and /register routes respectively.'
+
+@app.route('/login', methods=['POST'])
+def login():
+    pass
+
+@app.route('/register', methods=['POST'])
+def register():
+    pass
